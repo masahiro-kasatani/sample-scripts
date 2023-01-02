17ba6000a46ca7e44f95f26c0d2c7259dc7da11c
kasa <kasatani1226@gmail.com>
2023-01-02 17:26:24 +0900

    add 5-1

---
+++ 5-1/exp-path.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#任意の文字
+ls string.?
+ls string.???
+ls *.txt
+ls string*.c #空文字も含む
+
+#いずれか１文字
+ls string.[ch]
+
+#範囲指定
+ls file[1-3].txt
+
+#除外
+ls file[!24].txt
+
+#該当しない場合はエラーとなる
+#隠しファイルの「.」にはマッチしない
+ls *hidden
+
+ls .*en
